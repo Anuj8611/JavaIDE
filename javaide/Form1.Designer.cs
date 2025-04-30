9c40c65241697086198f48ea56434a29d2625a39
JUNA <JUNA@JUNA>
2025-04-30 09:27:16 +0530

    Initial commit - Java IDE built with C#, WinForms, ScintillaNET

---
+++ javaide/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿
+namespace javaide
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.txtCode = new ScintillaNET.Scintilla();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 324);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Output";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox2.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(12, 352);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(776, 86);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Save As..";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackgroundImage = global::javaide.Properties.Resources.images__2_;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button2.Location = new System.Drawing.Point(114, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(24, 23);
+            this.button2.TabIndex = 4;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackgroundImage = global::javaide.Properties.Resources.Basic_red_dot;
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button3.Location = new System.Drawing.Point(144, 4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(24, 23);
+            this.button3.TabIndex = 5;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // txtCode
+            // 
+            this.txtCode.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtCode.AutoCMaxHeight = 9;
+            this.txtCode.Lexer = ScintillaNET.Lexer.Cpp;
+            this.txtCode.Location = new System.Drawing.Point(15, 33);
+            this.txtCode.Name = "txtCode";
+            this.txtCode.Size = new System.Drawing.Size(773, 288);
+            this.txtCode.TabIndex = 6;
+            this.txtCode.Click += new System.EventHandler(this.txtCode_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtCode);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Juna IDE";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox scintilla1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private ScintillaNET.Scintilla txtCode;
+    }
+}
+
---
+++ javaide/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using ScintillaNET;
+using System.Diagnostics;
+
+
+namespace javaide
+{
+    public partial class Form1 : Form
+    {
+        private string javaFilePath = "Program.java";
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeEditorTheme();
+            txtCode.CharAdded += TxtCode_CharAdded;
+        }
+
+        private void TxtCode_CharAdded(object sender, CharAddedEventArgs e)
+        {
+            char c = (char)e.Char;
+
+            string closingChar = null;
+
+            switch (c)
+            {
+                case '(':
+                    closingChar = ")";
+                    break;
+                case '{':
+                    closingChar = "}";
+                    break;
+                case '[':
+                    closingChar = "]";
+                    break;
+                case '"':
+                    closingChar = "\"";
+                    break;
+                case '\'':
+                    closingChar = "'";
+                    break;
+            }
+
+
+            if (closingChar != null)
+            {
+                int currentPos = txtCode.CurrentPosition;
+                txtCode.InsertText(currentPos, closingChar);
+                txtCode.GotoPosition(currentPos); // Move cursor between the pair
+            }
+        }
+
+        private void InitializeEditorTheme()
+        {
+            txtCode.StyleResetDefault();
+            txtCode.Styles[Style.Default].Font = "Consolas";
+            txtCode.Styles[Style.Default].Size = 15;
+            txtCode.Styles[Style.Default].BackColor = Color.FromArgb(30, 30, 30);
+            txtCode.Styles[Style.Default].ForeColor = Color.FromArgb(220, 220, 220);
+            txtCode.StyleClearAll();
+
+            txtCode.Lexer = Lexer.Cpp;
+            
+
+            // Java Keywords
+            txtCode.SetKeywords(0,
+        "abstract assert break case catch class const continue default do else enum " +
+        "extends final finally for goto if implements import instanceof interface " +
+        "native new package private protected public return static strictfp super " +
+        "switch synchronized this throw throws transient try volatile while");
+
+            // Set type-related keywords (Style.Cpp.Word2 - GREENISH)
+            txtCode.SetKeywords(1,
+                "void int float double long short char boolean byte String true false null");
+
+            // Style definitions (VS Code Dark+ Theme Inspired)
+            txtCode.Styles[Style.Cpp.CommentLine].ForeColor = Color.FromArgb(106, 153, 85);
+            txtCode.Styles[Style.Cpp.Comment].ForeColor = Color.FromArgb(106, 153, 85);
+            txtCode.Styles[Style.Cpp.Number].ForeColor = Color.FromArgb(181, 206, 168);
+            txtCode.Styles[Style.Cpp.String].ForeColor = Color.FromArgb(206, 145, 120);
+            txtCode.Styles[Style.Cpp.Character].ForeColor = Color.FromArgb(206, 145, 120);
+            txtCode.Styles[Style.Cpp.Word].ForeColor = Color.FromArgb(86, 156, 214); // keywords
+            txtCode.Styles[Style.Cpp.Word2].ForeColor = Color.FromArgb(78, 201, 176); // teal-green
+            txtCode.Styles[Style.Cpp.Operator].ForeColor = Color.FromArgb(212, 212, 212);
+            txtCode.Styles[Style.Cpp.Identifier].ForeColor = Color.FromArgb(220, 220, 220);
+
+            // Line numbers
+            txtCode.Margins[0].Width = 30;
+            txtCode.Margins[0].BackColor = Color.FromArgb(45, 45, 45);
+
+            // Highlight current line
+            txtCode.CaretLineVisible = true;
+            txtCode.CaretLineBackColor = Color.FromArgb(40, 40, 40);
+
+            // Caret and selection
+            txtCode.CaretForeColor = Color.White;
+            txtCode.SetSelectionBackColor(true, Color.FromArgb(60, 60, 60));
+
+            // Optional: Indentation guides
+            txtCode.IndentationGuides = IndentView.LookBoth;
+
+            
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            File.WriteAllText(javaFilePath, txtCode.Text);
+            textBox2.Text = "Saved as " + javaFilePath;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            File.WriteAllText(javaFilePath, txtCode.Text);
+            ProcessStartInfo psi = new ProcessStartInfo("javac", javaFilePath);
+            psi.RedirectStandardOutput = true;
+            psi.RedirectStandardError = true;
+            psi.UseShellExecute = false;
+            psi.CreateNoWindow = true;
+
+            Process process = Process.Start(psi);
+            string output = process.StandardOutput.ReadToEnd();
+            string error = process.StandardError.ReadToEnd();
+            process.WaitForExit();
+
+            textBox2.Text = string.IsNullOrEmpty(error) ? "Compiled successfully!" : error;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ProcessStartInfo psi = new ProcessStartInfo("java", "Program");
+            psi.RedirectStandardOutput = true;
+            psi.RedirectStandardError = true;
+            psi.UseShellExecute = false;
+            psi.CreateNoWindow = true;
+
+            Process process = Process.Start(psi);
+            string output = process.StandardOutput.ReadToEnd();
+            string error = process.StandardError.ReadToEnd();
+            process.WaitForExit();
+
+            textBox2.Text = string.IsNullOrEmpty(error) ? output : error;
+        }
+
+        private void txtCode_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
